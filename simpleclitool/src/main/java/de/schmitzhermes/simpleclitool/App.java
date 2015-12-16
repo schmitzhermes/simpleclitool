a59cd2ee47697b637d7e4104bd342783070e3797
schmitzhermes <jan@schmitz-hermes.de>
2015-12-16 12:02:23 +0100

    compile level 1.7

--- simpleclitool/src/main/java/de/schmitzhermes/simpleclitool/App.java
+++ simpleclitool/src/main/java/de/schmitzhermes/simpleclitool/App.java
@@ -4,10 +4,6 @@ package de.schmitzhermes.simpleclitool;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+
 }
